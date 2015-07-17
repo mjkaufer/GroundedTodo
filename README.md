5eedb842954e65569405d970d08572185dd7471d
Matthew Kaufer <mjkaufer@gmail.com>
2015-07-17 11:20:33 -0400

    It works

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-# Simple Todo List
+# GroundedTodo
 
-The Meteor Tutorial app.
+A fork of the Meteor Tutorial app, which should have basic functionality offline, tracking and updating changes.
 
-Use it to share a single todo list with your friends. The list updates on everyone's screen in real time, and you can make tasks private if you don't want others to see them.
+Using GroundDB, AppCache, and some cool logic to retry any `Meteor.call`'s made.
 
-Learn how to build this app by following the [Meteor Tutorial](http://www.meteor.com/install).
+## Testing It
 
-![screenshot](screenshot.png)
+* Run meteor, make an account, and log in.
+* Add some things to your todo list.
+* Kill your meteor server. Try doing things
+	* Delete something from your todo list
+	* Change something from private to public
+
+Normally, you wouldn't be able to do something like remove an item when you're offline and have the changes persist to the server. Usually, you would see the local changes, but upon refreshing, your local collection would pull from the server's collection and your removal would be undone. With the `groundCall` method written in here, things are different. `groundCall` saves any `Meteor.call`'s made when offline, and fires them when you get online again. This is good because security is still preserved. Rather than blindly copy the client's database to the server's, which might overwrite changes and let the client use the database maliciously, we run the same secure `Meteor.call` methods invoked earlier.
--- simple-todos.js
+++ simple-todos.js
@@ -1,10 +1,13 @@
 Tasks = new Mongo.Collection("tasks");
+Ground.Collection(Tasks);//saves the contents of Tasks offline
+
+CallQueue = new Ground.Collection(null)//this database tracks any meteor calls made while the user was offline, and retries them later
 
 if (Meteor.isServer) {
   // This code only runs on the server
   // Only publish tasks that are public or belong to the current user
   Meteor.publish("tasks", function () {
-    return Tasks.find({
+    return Tasks.find({//we only can access this if we're online in the first place, so we'll use the online db here
       $or: [
         { private: {$ne: true} },
         { owner: this.userId }
@@ -13,7 +16,58 @@ if (Meteor.isServer) {
   });
 }
 
+
+
 if (Meteor.isClient) {
+
+  var lastState = null
+
+  Deps.autorun(function(){//to check if the user's connected state has changed and, if it has and the user is online, execute calls
+
+    if(lastState == Meteor.status().connected || !Meteor.status().connected)//if the user has not changed connected states, or if they're offline, don't execute the queued meteor calls
+      return
+
+    if(CallQueue.find().count() == 0)//if there's nothing in the callqueue, don't do anything either
+      return
+
+    lastState = Meteor.status().connected//update lastState 
+
+    CallQueue.find().forEach(function(doc){//iterate through all queued meteor calls
+      
+      Meteor.apply(doc.methodName, doc.args, doc.meteorCallCallback)//run the queued call method
+      CallQueue.remove(doc._id)//remove from collection
+
+    })
+
+  })
+
+  /*
+    groundCall is our wrapper for Meteor.call
+    Instead of doing Meteor.call(method, arg1, arg2, arg3, callback),
+    you do
+    groundCall(method, [arg1, arg2, arg3], callback)
+  */
+
+  function groundCall(methodName, argArray, callback){
+
+    var meteorCallCallback = function(e, r){// called when meteor call triggers its callback
+
+      if(!Meteor.status().connected){
+        console.log("Call method requires internet access - will retry when connection gets better")
+      }
+
+      if(callback && typeof callback == "function")
+        callback(e, r)//the user's callback, if provided
+    }
+
+    Meteor.apply(methodName, argArray, meteorCallCallback)//try to call the method
+    //we call this regardless if the user is offline or not, so the user sees changes on their side
+
+    if(!Meteor.status().connected)//if the user is offline when the method is attempted, push it to the queue
+      CallQueue.insert({args: argArray, methodName: methodName, meteorCallCallback: meteorCallCallback})
+
+  }
+
   // This code only runs on the client
   Meteor.subscribe("tasks");
 
@@ -44,7 +98,10 @@ if (Meteor.isClient) {
       var text = event.target.text.value;
 
       // Insert a task into the collection
-      Meteor.call("addTask", text);
+      // Meteor.call("addTask", text);
+      groundCall('addTask',[text], function(e, r){
+        console.log("Added task", text)
+      })
 
       // Clear form
       event.target.text.value = "";
@@ -63,13 +120,22 @@ if (Meteor.isClient) {
   Template.task.events({
     "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
-      Meteor.call("setChecked", this._id, ! this.checked);
+      // Meteor.call("setChecked", this._id, ! this.checked);
+      groundCall('setChecked', [this._id, !this.checked], function(e, r){
+        console.log("Toggled checked")
+      })
     },
     "click .delete": function () {
-      Meteor.call("deleteTask", this._id);
+      // Meteor.call("deleteTask", this._id);
+      groundCall('deleteTask', [this._id], function(e, r){
+        console.log("Deleted task")
+      })
     },
     "click .toggle-private": function () {
-      Meteor.call("setPrivate", this._id, ! this.private);
+      // Meteor.call("setPrivate", this._id, ! this.private);
+      groundCall('setPrivate', [this._id, !this.private], function(e, r){
+        console.log("Set private")
+      })
     }
   });
 
@@ -81,43 +147,59 @@ if (Meteor.isClient) {
 Meteor.methods({
   addTask: function (text) {
     // Make sure the user is logged in before inserting a task
+
     if (! Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
     }
 
-    Tasks.insert({
+    return Tasks.insert({
       text: text,
       createdAt: new Date(),
       owner: Meteor.userId(),
       username: Meteor.user().username
-    });
+    })
+
+
   },
   deleteTask: function (taskId) {
+
+
     var task = Tasks.findOne(taskId);
+
     if (task.private && task.owner !== Meteor.userId()) {
       // If the task is private, make sure only the owner can delete it
       throw new Meteor.Error("not-authorized");
     }
 
-    Tasks.remove(taskId);
+    return Tasks.remove(taskId);
+
   },
   setChecked: function (taskId, setChecked) {
+
     var task = Tasks.findOne(taskId);
     if (task.private && task.owner !== Meteor.userId()) {
       // If the task is private, make sure only the owner can check it off
       throw new Meteor.Error("not-authorized");
     }
 
-    Tasks.update(taskId, { $set: { checked: setChecked} });
+    return Tasks.update(taskId, { $set: { checked: setChecked} });
+
   },
   setPrivate: function (taskId, setToPrivate) {
+
     var task = Tasks.findOne(taskId);
 
     // Make sure only the task owner can make a task private
+    console.log("Great success")
     if (task.owner !== Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
     }
 
-    Tasks.update(taskId, { $set: { private: setToPrivate } });
+    return Tasks.update(taskId, { $set: { private: setToPrivate } });
+
+  },
+  test: function(a){
+    console.log("HEY");
+    return a;
   }
 });
